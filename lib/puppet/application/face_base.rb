a3c034092789ec01554da651e55e7b0ed7d89450
Josh Cooper <joshcooper@users.noreply.github.com>
2021-04-16 17:17:14 -0700

    (maint) Resolve Lint/RedundantRequireStatement

--- lib/puppet/application/face_base.rb
+++ lib/puppet/application/face_base.rb
@@ -1,7 +1,6 @@
 require_relative '../../puppet/application'
 require_relative '../../puppet/face'
 require 'optparse'
-require 'pp'
 
 class Puppet::Application::FaceBase < Puppet::Application
   option("--debug", "-d") do |arg|
