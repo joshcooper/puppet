98b236b724eeacc401be014f93cf849640c4e5f2
Josh Cooper <joshcooper@users.noreply.github.com>
2021-04-19 20:44:19 -0700

    (maint) Resolve Lint/SelfAssignment

--- lib/puppet/pops/evaluator/runtime3_support.rb
+++ lib/puppet/pops/evaluator/runtime3_support.rb
@@ -479,8 +479,6 @@ module Runtime3Support
 
   # Configure the severity of failures
   class SeverityProducer < Validation::SeverityProducer
-    Issues = Issues
-
     def initialize
       super
       p = self
