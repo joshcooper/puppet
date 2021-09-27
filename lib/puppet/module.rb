1996a279af1b868a1693b251c2d768143d23a748
Josh Cooper <joshcooper@users.noreply.github.com>
2021-09-27 08:38:25 -0700

    (TBD) Freeze all the strings

--- lib/puppet/module.rb
+++ lib/puppet/module.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'puppet/util/logging'
 require 'puppet/module/task'
 require 'puppet/module/plan'
--- lib/puppet/pops/puppet_stack.rb
+++ lib/puppet/pops/puppet_stack.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'puppet/thread_local'
 
 module Puppet
